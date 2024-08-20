9656be9d3bfabafa0f9c2934ee37f3e005854325
Peerawat Ruangwattanakit <peerawat.ruang@loolootech.com>
2024-08-20 20:09:43 +0700

    feat: add header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
-<script setup lang="ts">
+<script>
+import Header from "@/components/Header.vue";
+export default {
+  name: "App",
+  components: {
+    Header
+  }
+};
 </script>
 
 <template>
+  <Header />
+  <router-view></router-view>
 </template>
 
 <style>
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="bg-white border-b border-gray-200 fixed top-0 left-0 right-0 z-50">
+    <div class="container mx-auto flex justify-between items-center py-4">
+      <router-link class="text-xl font-bold text-gray-900" :to="{ name: 'home' }">
+        James Airline
+      </router-link>
+      <ul class="flex space-x-6">
+        <li>
+  <router-link :to="{ name: 'login' }" class="text-gray-700 hover:text-gray-900">
+    <i class="ion-compose"></i> Login/Register
+  </router-link>
+
+  </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+
+export default {
+  name: "Header",
+};
+</script>
